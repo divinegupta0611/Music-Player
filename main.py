b256af6b2c932ad593e791f33205a7ebdfc42fc6
divinegupta0611 <guptadivine0611@gmail.com>
2024-12-02 23:45:53 +0530

    Main File

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import requests
 from tkinter import Tk
+import tkinter as tk
 import pygame
 import os
 
+#create main window
+root = tk.Tk()
+root.title("Tkinter frame example")
+root.geometry("400x300")
+
+root.mainloop()
